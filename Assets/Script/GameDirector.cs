30e6d4cf60cf8435f2038953ba4834c9354c42fa
tomo <>
2024-07-09 15:21:42 +0900

    timerの実装

--- Assets/Script/GameDirector.cs
+++ Assets/Script/GameDirector.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameDirector : MonoBehaviourPunCallbacks
 {
+    GameObject timerText;
+    float time=0.0f;
+    protected void Start(){
+        this.timerText = GameObject.Find("Timer");
+    }
     // Update is called once per frame
     void Update()
-    {    PhotonNetwork.AutomaticallySyncScene = true; // trueならマスタークライアントと同期
+    {   
+        this.time += Time.deltaTime;
+        PhotonNetwork.AutomaticallySyncScene = true; // trueならマスタークライアントと同期
         bool goalflag=PlayerController.goalflag;
         // goalflagがtrueならゴールシーンに遷移
         if(goalflag){
             SceneManager.LoadSceneAsync("Result",LoadSceneMode.Single);
-        }   
+        }
+        this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
     }
 }
